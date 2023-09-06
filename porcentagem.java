e20377b161d67fc5a5da96a841544652f0564321
Eduardo <eduardodacosta162003@gmail.com>
2023-09-06 19:54:35 -0300

    porcentagem

---
+++ porcentagem.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class porcentagem {
+    public static void main(String[] args){
+       int total, score; 
+       float percentage;
+       Scanner inputNumScanner = new Scanner(System.in);
+       
+       System.out.println("Digite o valor total ou máximo: ");       
+       total = inputNumScanner.nextInt();
+       
+       System.out.println("Digite o valor obtido: ");
+       score = inputNumScanner.nextInt();
+       
+       percentage = (score * 100/ total);
+       
+       System.out.println("A porcentagem é = " + percentage + " %");
+    }
+}
